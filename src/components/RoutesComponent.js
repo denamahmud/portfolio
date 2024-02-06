62ad944b5f30e0296ad460ba0e5427f8e3139f2d
denamahmud <denamahmud>
2024-02-06 12:38:44 +0300

    added hashrouter

--- src/components/RoutesComponent.js
+++ src/components/RoutesComponent.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Crud from "../pages/Crud";
 import Movies from "../pages/Movies";
@@ -7,15 +7,15 @@ import Fitness from "../pages/Fitness";
 
 function RoutesComponent() {
   return (
-    <BrowserRouter> 
-      <HashRouter>
+    <HashRouter> 
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/crud" element={<Crud />} />
         <Route path="/movies" element={<Movies />} />
         <Route path="/weather" element={<Weather />} />
         <Route path="/fitness" element={<Fitness />} />
-      </HashRouter>
-    </BrowserRouter>
+      </Routes>
+    </HashRouter>
   );
 }
 
